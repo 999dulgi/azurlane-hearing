a8418b07230e75c393bab46883bf0c81adbc3b5f
999dulgi <rlawhdals0501@gmail.com>
2025-09-05 12:55:12 +0900

    init project

---
+++ src/app/ships/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Box, Container } from "@mui/material";
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from "@mui/material";
+
+export default function Page() {
+    return (
+        <Box>
+            <Container>
+                <h1>Ships</h1>
+            </Container>
+        </Box>
+    );
+}
